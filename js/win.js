3fa70ccd1655f00f27a5f58c752e391cf58df8ed
gglyptodon <janina.mass@gmail.com>
2017-07-30 16:10:34 +0200

    end screen

--- js/win.js
+++ js/win.js
@@ -1,11 +1,11 @@
 var winState = {
     create: function(){
         // set fail timer to 0, so a refresh works as if from start
-        localStorage.setItem("escapepot_localstoragetimer", 0);
+        localStorage.setItem("turtlegames_localstoragetimer", 0);
 
         background = game.add.tileSprite(0, 0, 800, 600, 'backgroundwin');
         var style = { font: "bold 32px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
-        placeholdertext = game.add.text(50,50,"Whaaa?? you mixed your\npotion correctly\nsomething exciting\nhappens and\nyou're free", style);
+        placeholdertext = game.add.text(50,50,"GAME OVER ", style);
 
         button = game.add.button(game.world.centerX/2, 400, 'buttonrestart', actionOnClick, this, 1, 0, 2);
         button.onInputUp.add(this.backtostart, this);
