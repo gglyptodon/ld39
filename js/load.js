d6ebffb7bbf6d01e5fce6b94d19f880c85699628
frogsicle <a7denton@gmail.com>
2017-07-30 18:40:45 +0200

    major file structural changes

--- js/load.js
+++ js/load.js
@@ -2,7 +2,7 @@ var loadState = {
     preload: function(){
         // buttons
         game.load.spritesheet('buttonmenu', 'assets/btn/button_menu.png', 96, 32);
-        game.load.spritesheet('buttonredo', 'assets/btn/button_redo.png', 96, 32);
+//        game.load.spritesheet('buttonredo', 'assets/btn/button_redo.png', 96, 32);
         game.load.spritesheet('buttonrestart', 'assets/btn/button_restart.png', 96, 32);
         game.load.spritesheet('buttonmix', 'assets/btn/button_mix.png', 96, 32);
         //
@@ -11,7 +11,7 @@ var loadState = {
 
         // backgrounds
         game.load.image('backgroundmenu','assets/img/bg_menu.png');
-        game.load.image('backgroundredo','assets/img/bg_redo.png');
+//        game.load.image('backgroundredo','assets/img/bg_redo.png');
         game.load.image('backgroundwin', 'assets/img/bg_win.png')
         game.load.image('backgroundplay', 'assets/img/bg_play.png')
         // load fonts
--- js/turtlegames.js
+++ js/turtlegames.js
@@ -5,7 +5,7 @@ game.state.add('boot', bootState);
 game.state.add('load', loadState);
 game.state.add('menu', menuState);
 game.state.add('play', playState);
-game.state.add('redo', redoState);
+//game.state.add('redo', redoState);
 game.state.add('win', winState);
 
 game.state.start('boot');
