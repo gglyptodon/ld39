63ee59e4d4960f4a7884053196c8a17ba6122a7d
gglyptodon <janina.mass@gmail.com>
2017-07-30 11:40:06 +0200

    prepare adding assets

--- js/load.js
+++ js/load.js
@@ -5,9 +5,9 @@ var loadState = {
         game.load.spritesheet('buttonredo', 'assets/btn/button_redo.png', 96, 32);
         game.load.spritesheet('buttonrestart', 'assets/btn/button_restart.png', 96, 32);
         game.load.spritesheet('buttonmix', 'assets/btn/button_mix.png', 96, 32);
-        // stir direction buttons
-        game.load.spritesheet('cw', 'assets/btn/cw.png', 32, 32);
-        game.load.spritesheet('ccw', 'assets/btn/ccw.png', 32, 32);
+        //
+        //game.load.spritesheet('cw', 'assets/btn/cw.png', 32, 32);
+        //game.load.spritesheet('ccw', 'assets/btn/ccw.png', 32, 32);
 
         // backgrounds
         game.load.image('backgroundmenu','assets/img/bg_menu.png');
@@ -15,6 +15,22 @@ var loadState = {
         game.load.image('backgroundwin', 'assets/img/bg_win.png')
         game.load.image('backgroundplay', 'assets/img/bg_play.png')
 
+        // sprite sheets for characters
+        game.load.spritesheet('tortuga_small', 'assets/tortuga_hide.png', 68, 47);
+
+        //items
+        game.load.spritesheet('recharge', 'assets/recharge.png', 68, 47);
+
+        //music and soundFX TODO
+        //game.load.audio('bgmusic', ['assets/sounds/bgmusic.ogg']);
+        //game.load.audio('recharge_snd', ['assets/sounds/recharge.ogg']);
+        //game.load.audio('battery_snd', ['assets/sounds/recharge.ogg']);
+        //game.load.audio('run_snd',['assets/sounds/run_snd.ogg']);
+        //game.load.audio('reachstop_snd',['assets/sounds/reachstop_snd.ogg']);
+        //game.load.audio('gameover_snd',['assets/sounds/gameover_snd.ogg']);
+        //game.load.audio('gamestart_snd',['assets/sounds/gamestart_snd.ogg']);
+        game.load.audio('explode_snd',['assets/sounds/explosion_snd.ogg']);
+        game.load.audio('hallo'_snd,['assets/sounds/hallo_snd.ogg']);
     },
 
     create: function(){
