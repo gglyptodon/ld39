514a67598a8d5aa868a3e439286d5cb9dd1e3ca3
frogsicle <a7denton@gmail.com>
2017-07-30 18:01:00 +0200

    high score debug

--- js/win.js
+++ js/win.js
@@ -1,13 +1,13 @@
 var winState = {
     create: function(){
 
-        var highscore= localStorage.getItem("turtlegames_highscore");
-        var yourscore = localStorage.getItem("turtlegames_yourscore");
+        var highscore= +localStorage.getItem("turtlegames_highscore");
+        var yourscore = +localStorage.getItem("turtlegames_yourscore");
 
         background = game.add.tileSprite(0, 0, 800, 600, 'backgroundwin');
         var style = { font: "bold 32px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
         if (yourscore >= highscore){
-            placeholdertext = game.add.text(50,50,"GAME OVER \nNEW HIGHSCORE:  "+ highscore + " \\o/", style);
+            placeholdertext = game.add.text(50,50,"GAME OVER \nNEW HIGHSCORE:  "+ yourscore + " \\o/", style);
             localStorage.setItem("turtlegames_highscore", yourscore);
         }
         else{
