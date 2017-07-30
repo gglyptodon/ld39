9c9a3a43b6126adcb8b0b4adb51ac34c3c290886
frogsicle <a7denton@gmail.com>
2017-07-30 17:15:59 +0200

    graphics for menu and win

--- js/win.js
+++ js/win.js
@@ -7,7 +7,7 @@ var winState = {
         var style = { font: "bold 32px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
         placeholdertext = game.add.text(50,50,"GAME OVER ", style);
 
-        button = game.add.button(game.world.centerX/2, 400, 'buttonrestart', actionOnClick, this, 1, 0, 2);
+        button = game.add.button(game.world.centerX, game.world.height * .8, 'buttonrestart', actionOnClick, this, 1, 0, 2);
         button.onInputUp.add(this.backtostart, this);
     },
 
